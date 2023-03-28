87d0282c8b237f7b485784a8f059c49bcbc0635c
TijanJackson <tijanj02@gmail.com>
2023-03-28 15:42:06 +0200

    Added comments to all files for HDT

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ let buttonOneProgressCheck = true;
 let progressBtnOne = document.getElementById("progressBtnOne");
 let tick = document.getElementById("tickOne");
 
+//Vanilla js function to adjust styling of progress buttons if boolean value is true
 function buttonCheckFunc () {
     if (buttonOneProgressCheck == true) {
         tick.classList.remove('d-none');
         progressBtnOne.classList.add('active-btn');
     }
 }
+
+// Multiple jquery functions to toggle display classes
 
 $("#menu-toggle").click(function(){
     $(".toggle").toggle();
